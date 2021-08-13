793eadfa3031f2f93de4f400870c924603c060a9
Seira Yun <discoverdeepseira@gmail.com>
2021-08-13 16:57:06 +0900

    root added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'bookmarks/new'
-  get 'bookmarks/create'
-  get 'bookmarks/destroy'
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
+  root to: 'lists#index'
 end
